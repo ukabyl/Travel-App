0ee904f22217e73ffad0f1248ff80d5504e266d4
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2020-07-09 22:35:20 +0500

    Refactor functions to arrow functions in utils.js

--- app/client/scripts/utils.js
+++ app/client/scripts/utils.js
@@ -1,43 +1,28 @@
-function upperCaseArray(text) {
-    var result = text.replace(/([A-Z]+)/g, ",$1").replace(/^,/, "");
-    return result.split(",");
-}
+const upperCaseArray = (text) => text.replace(/([A-Z]+)/g, ",$1").replace(/^,/, "").split(",");
 
-function _mlsToDays(mls) {
-    return (mls / (60*60*24*1000)) % 365;
-}
+const _mlsToDays = (mls) => (mls / (60*60*24*1000)) % 365;
 
-function _quantityOfDays(startDate, endDate) {
-    const mls = Date.parse(endDate) - Date.parse(startDate);
-    return _mlsToDays(mls);
-}
+const _quantityOfDays = (startDate, endDate) => _mlsToDays(Date.parse(endDate) - Date.parse(startDate));
 
-function daysLeft(startDate) {
-    const mls = Date.parse(startDate) - Date.now();
-    return Math.ceil(_mlsToDays(mls));
-}
+const daysLeft = (startDate) => Math.ceil(_mlsToDays(Date.parse(startDate) - Date.now()));
 
-function transformData({ weather, image, flag, geoNames, data }) {
-    if(!weather) {
-        return {};
-    }
-    return {
-        departure_from: data.fromLocation,
-        departure_point: geoNames.name,
-        country: geoNames.countryName,
-        departure_time: data.startDate,
-        return: data.endDate,
-        time_relax: _quantityOfDays(data.startDate, data.endDate),
-        days_left: daysLeft(data.startDate),
-        image: image.hits[0] ? image.hits[0].largeImageURL : '',
-        flag: flag,
-        sunrise: weather[0].sunrise,
-        sunset: weather[0].sunset,
-        temp: weather[0].app_temp,
-        weather_desc: weather[0].weather.description
-        
-    }
-}
+const transformData = ({ weather, image, flag, geoNames, data }) => ({
+    departure_from: data.fromLocation,
+    departure_point: geoNames.name,
+    country: geoNames.countryName,
+    departure_time: data.startDate,
+    return: data.endDate,
+    time_relax: _quantityOfDays(data.startDate, data.endDate),
+    days_left: daysLeft(data.startDate),
+    image: image.hits[0] ? image.hits[0].largeImageURL : '',
+    flag: flag,
+    sunrise: weather[0].sunrise,
+    sunset: weather[0].sunset,
+    temp: weather[0].app_temp,
+    weather_desc: weather[0].weather.description
+    
+});
+
 
 export {
     upperCaseArray,
