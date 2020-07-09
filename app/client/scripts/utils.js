cd54254a76479f7123ede6ed64c4fc4cc7fa252b
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2020-07-10 01:57:44 +0500

    Add one test for checking whether server is running or not

--- app/client/scripts/utils.js
+++ app/client/scripts/utils.js
@@ -10,23 +10,29 @@ const _quantityOfDays = (startDate, endDate) => _mlsToDays(Date.parse(endDate) -
 // Count quantity of days from current date to start date
 const daysLeft = (startDate) => Math.ceil(_mlsToDays(Date.parse(startDate) - Date.now()));
 
-// Transform form data for server request and save in this way
-const transformData = ({ weather, image, flag, geoNames, data }) => ({
-    departure_from: data.fromLocation,
-    departure_point: geoNames.name,
-    country: geoNames.countryName,
-    departure_time: data.startDate,
-    return: data.endDate,
-    time_relax: _quantityOfDays(data.startDate, data.endDate),
-    days_left: daysLeft(data.startDate),
-    image: image.hits[0] ? image.hits[0].largeImageURL : '',
-    flag: flag,
-    sunrise: weather[0].sunrise,
-    sunset: weather[0].sunset,
-    temp: weather[0].app_temp,
-    weather_desc: weather[0].weather.description
+// Transform form data for server request and save in this 
+const transformData = ({ weather, image, flag, geoNames, data }) => {
+    if(!weather) {
+        return {}
+    }
     
-});
+    return {
+        departure_from: data.fromLocation,
+        departure_point: geoNames.name,
+        country: geoNames.countryName,
+        departure_time: data.startDate,
+        return: data.endDate,
+        time_relax: _quantityOfDays(data.startDate, data.endDate),
+        days_left: daysLeft(data.startDate),
+        image: image.hits[0] ? image.hits[0].largeImageURL : '',
+        flag: flag,
+        sunrise: weather[0].sunrise,
+        sunset: weather[0].sunset,
+        temp: weather[0].app_temp,
+        weather_desc: weather[0].weather.description
+    }
+    
+};
 
 
 export {
---
+++ app/server/server.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+
+
+describe('The page should be running on localhost 8080', () => {
+    test('Page response successfully.', async () => {
+        const res = await request('http://localhost:8080').get('/');
+        expect(res.statusCode).toBe(200);
+    });
+});
