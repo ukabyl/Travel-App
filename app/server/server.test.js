572cba1b68e4f7f5e0b36d5d7609bca1a6ac974e
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2020-07-15 13:39:15 +0500

    Add comment in server.js file

--- app/server/server.test.js
+++ app/server/server.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 
-
+// Test for server listening
 describe('The page should be running on localhost 8080', () => {
     test('Page response successfully.', async () => {
         const res = await request('http://localhost:8080').get('/');
